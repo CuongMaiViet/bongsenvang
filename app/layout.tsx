b5a999df43a88d1f672560ab80452ea47cd37520
CuongMaiViet <l9931455@gmail.com>
2024-06-12 11:56:00 +0700

    add vercel analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Noto_Serif } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const fontFamily = Noto_Serif({
@@ -29,6 +30,7 @@ export default function RootLayout({
         <body className={fontFamily.variable}>
           {children}
           <SpeedInsights />
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
